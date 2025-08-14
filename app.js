40a44a12bda48cf7ee6f63b5443074847926ff62
R3DRUM <yael.esime@gmail.com>
2025-08-13 21:07:39 -0600

    Crea el app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigos = [];
+let amigo = "";
+
+function agregarAmigo(){
+    listaAmigos.push(document.getElementById('amigo').value);
+    alert(listaAmigos)
+}
+
+
